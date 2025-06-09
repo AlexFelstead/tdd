084c52a2beaa7fc6b3bea99997e9860214157f40
Alex Felstead <alex.felstead@gmail.com>
2025-06-09 19:49:26 +0100

    new exercise - counter

---
+++ tests/test_counter.py
@@ -0,0 +1,16 @@
+from lib.counter import *
+
+def testing_counter_adds():
+    counter = Counter()
+    result = counter.add(10)
+    assert "Counted to 10 so far."
+    
+def testing_counter_starts_at_0():
+    counter = Counter()
+    assert counter.count == 0
+
+def testing_counter_counts_multiple():
+    counter = Counter()
+    result = counter.add(10)
+    result = counter.add(5)
+    assert "Counted to 15 so far."
